6e46ee986e8238102396f9385b795546e2b63afe
Enzo Marzo <enzomarzo@hotmail.com>
2025-02-13 10:58:44 +0000

    feat: add error handling when aem is not started

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -1,33 +1,39 @@
 /* eslint-disable indent */
-import express from 'express';
-import { StaticRouter } from 'react-router-dom';
-import ReactDOMServer from 'react-dom/server';
-import App from '../client/components/App.jsx';
-import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from '../createEmotionCache';
-import { ThemeProvider } from '@mui/material/styles';
-import christmasTheme from '../themes/christmasTheme';
-import { CacheProvider } from '@emotion/react';
-import React from 'react';
+import express from "express";
+import { StaticRouter } from "react-router-dom";
+import ReactDOMServer from "react-dom/server";
+import App from "../client/components/App.jsx";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../createEmotionCache";
+import { ThemeProvider } from "@mui/material/styles";
+import christmasTheme from "../themes/christmasTheme";
+import { CacheProvider } from "@emotion/react";
+
+import React from "react";
 
 const app = express();
-app.use('/static', express.static(__dirname));
+app.use("/static", express.static(__dirname));
 const PORT = process.env.PORT || 3001;
 const aemUrl =
-  'http://localhost:4502/content/arrow-dxp/language-masters/en/home.html';
+  "http://localhost:4502/content/arrow-dxp/language-masters/en/home.html";
 
 const renderReactApp = async (location, productId = null) => {
   const emotionCache = createEmotionCache();
+  let errorMsg = null;
   const { extractCriticalToChunks, constructStyleTagsFromChunks } =
     createEmotionServer(emotionCache);
 
   const aemAuth = {
-    Authorization: 'Basic ' + Buffer.from('admin:admin').toString('base64'),
+    Authorization: "Basic " + Buffer.from("admin:admin").toString("base64"),
   };
 
   const fetchHtml = async (url, headers) => {
-    const response = await fetch(url, { headers });
-    return response.text();
+    try {
+      const response = await fetch(url, { headers });
+      return response.text();
+    } catch (error) {
+      errorMsg = "Please start AEM locally on port 4502 to proceed";
+    }
   };
 
   const fetchJson = async (url) => {
@@ -36,15 +42,6 @@ const renderReactApp = async (location, productId = null) => {
   };
 
   const aemHomeHtml = await fetchHtml(aemUrl, aemAuth);
-
-  // Add <style></style> tag's to the head section
-  const aemHomeHtmlWithStyle = aemHomeHtml.replace(
-    '</head>',
-    `
-    <style></style>
-    </head>
-  `
-  );
 
   const productDetails = productId
     ? await fetchJson(`https://api.escuelajs.co/api/v1/products/${productId}`)
@@ -66,29 +63,31 @@ const renderReactApp = async (location, productId = null) => {
   const emotionChunks = extractCriticalToChunks(reactApp);
   const emotionCss = constructStyleTagsFromChunks(emotionChunks);
 
-  const reactHtml = aemHomeHtmlWithStyle
-    .replace('<style></style>', emotionCss)
-    .replace(
-      '<div id="spa-root"></div>',
-      `<div id="root">${reactApp}</div>
+  const reactHtml =
+    aemHomeHtml &&
+    aemHomeHtml
+      .replace("</head>", `<style>${emotionCss}</style></head>`)
+      .replace("<body", '<script defer src="/static/client.js"></script></body')
+      .replace(
+        '<div id="spa-root"></div>',
+        `<div id="root">${reactApp}</div>
     <script>window.__INITIAL_DATA__ = ${JSON.stringify({
       productId,
       productDetails,
     })};</script>`
-    );
+      );
 
-  return reactHtml.replace(
-    '<body',
-    '<script defer src="/static/client.js"></script></body'
-  );
+  if (errorMsg) return errorMsg;
+
+  return reactHtml;
 };
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const indexHtml = await renderReactApp(req.url);
   res.status(200).send(indexHtml);
 });
 
-app.get('/products/:id', async (req, res) => {
+app.get("/products/:id", async (req, res) => {
   const productId = req.params.id;
   const indexHtml = await renderReactApp(req.url, productId);
   res.status(200).send(indexHtml);
